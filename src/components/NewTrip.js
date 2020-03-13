7e47b346bb12f7fd3246d48e85af48572ee663fd
Kyle <kylepcole@gmail.com>
2020-03-13 16:27:02 -0400

    newTripForm created

--- src/components/NewTrip.js
+++ src/components/NewTrip.js
@@ -1,15 +1,43 @@
 import React from 'react'
-
+import TripModal from './TripModal.js'
 
 class NewTrip extends React.Component {
+	state = {
+		search: null,
+		showTripModal: false
+	}
+
+	toggleTripModal = () => {
+		this.setState({
+			showTripModal: !this.state.showTripModal
+		})
+	}
+
+	handleNewTripSearchFormSubmit = (event) =>{
+		event.preventDefault()
+		this.toggleTripModal()
+		event.target.reset()
+	}
+
+	handleSearchFieldChange(event){
+		this.setState({
+			[event.target.name]: event.target.value
+		})
+	}
+
 	render (){
 		return (
-			<React.Fragment>
-				<form>
+			<div id="new-trip-container">
+				<h1>Where to?</h1>
+				<form 
+					onSubmit={ this.handleNewTripSearchFormSubmit }
+					onChange={ event => this.handleSearchFieldChange(event) }
+				>
 					<input name="search" placeholder="Bali, Indonesia"></input>
 					<button type="submit" name='submit' >GO!</button>
 				</form>
-			</React.Fragment>
+				<TripModal status={ this.state.showTripModal } close={ this.toggleTripModal } search={ this.state.search }/>
+			</div>
 		)
 	}
 }
